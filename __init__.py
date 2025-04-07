a3fa2bbd83ad265c381e9881eba1ccba09162fb9
murielleblondelle <muriellepetako@gmail.com>
2025-04-07 10:38:22 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import json                                                                                                                                     
-app = Flask(__name__)                                              hjfrom flask import Flask
+app = Flask(__name__)                                              
 from flask import render_template
 from flask import json
 
