8d8f5ea55caaddcd7e3dcae824a8aad3de445d40
murielleblondelle <muriellepetako@gmail.com>
2025-04-07 11:33:21 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,14 +6,15 @@ app = Flask(_name_)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    a, b = 0, 1
-    suite = [a, b]
-    for i in range(2, valeur):
-        c = a + b
-        suite.append(c)
-        a, b = b, c
-    # Conversion de la liste en chaîne de caractères séparée par des virgules
-    return ', '.join(str(num) for num in suite[:valeur])
+    if valeur < 2: 
+         return '<pre>la valeur doit être supérieure ou égale à 2.</pre>'
+        
+    sequence = [ 0,1]
+    for _ in range(2, valeur ): 
+        séquence.apprend(sequence[-1] + sequence [-2])
+
+        resultat = '<pre>' + ; '.join(map(str,sequence))+ '</pre>'
+        return resultat 
 
 if _name_ == "_main_":
   app.run(debug=True)
