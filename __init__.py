21f61bbbecbe8a519d6ddb752ae49056abe12569
murielleblondelle <muriellepetako@gmail.com>
2025-04-07 15:11:08 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,18 @@
-n = int(input("Combien de nombres souhaitez-vous entrer ? "))
-max_value = None
 
-for i in range(n):
-    num = int(input(f"Entrez le nombre {i + 1}: "))
-    
-    if max_value is None:
-        max_value = num
-    elif num > max_value:
-        max_value = num
+  from flask import Flask
 
-print("La valeur maximale est :", max_value)
-                                                          
+app = Flask(__name__)
+
+@app.route('/<path:valeurs>')
+def exercice(valeurs):
+    liste_nombres = valeurs.split('/')
+    liste_nombres = [int(n) for n in liste_nombres]
+
+    for i in range(len(liste_nombres) - 1):
+        if liste_nombres[i] > liste_nombres[i + 1]:
+            liste_nombres[i + 1] = liste_nombres[i]
+
+    return f"Le nombre maximum est : {liste_nombres[-1]}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')                                                        
